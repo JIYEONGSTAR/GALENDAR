24c548c4290b00821fc31cb3db918ba8e731baf3
JIYEONGSTAR <jiyeongstar@gmail.com>
2020-06-20 14:43:16 +0900

    makeList

---
+++ test/src/devdog/jdbc/Member.java
@@ -0,0 +1,34 @@
+package devdog.jdbc;
+
+public class Member {
+	int id;
+	String name;
+	int studentID;
+	String birth;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getStudentID() {
+		return studentID;
+	}
+	public void setStudentID(int studentID) {
+		this.studentID = studentID;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+
+}
